b6466982d773a07ac5496b2da3339a4309a1546d
DhruvGodambe <dhruvrockstone@gmail.com>
2023-04-10 02:04:36 +0530

    ready to deploy

--- src/config/Chains/index.tsx
+++ src/config/Chains/index.tsx
@@ -1,21 +1,12 @@
 import {avalancheFuji, goerli, polygonMumbai, polygon, mainnet } from "@wagmi/core/chains";
-import { AvaxChain } from './AvaxChain';
-import { FujiChain } from './FujiChain';
-import { BinanceMainnetChain } from './BinanceMainnetChain';
-import { BinanceTestnetChain } from './BinanceTestnetChain';
 
 export const allChains = [
-  goerli,
   mainnet,
   polygon,
-  polygonMumbai,
-  AvaxChain,
-  FujiChain,
-  BinanceMainnetChain,
-  BinanceTestnetChain
+  polygonMumbai
 ];
 export const testnetChains = [ polygonMumbai];
-export const mainnetChains = [mainnet, polygon, AvaxChain, BinanceMainnetChain];
+export const mainnetChains = [mainnet, polygon];
 
 export const appChains = {
   allChains,
